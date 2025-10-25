d6337f8216b0e9f901d852545d7d9821f9ca80d4
Maik648 <maikpankow@gmail.com>
2025-10-25 16:06:47 +0200

    add funtion trash node remov

--- globel.js
+++ globel.js
@@ -1,6 +1,16 @@
 const mainConten = document.getElementById("main-content");
+const trashConten = document.getElementById("trash-content");
+let noteInput = document.getElementById("lnote");
+let titelInput = document.getElementById("ltitle");
+
 let notes = [];
-let noteInput = document.getElementById("lvorname");
+let titelNots = [];
+
+let trashNoats = [];
+let trashNoatsTitels = [];
+
+let newtrashNoats = [];
+let newtrashNoatsTitels = [];
 
 function init() {
   return renderNodes();
@@ -13,18 +23,47 @@ function renderNodes() {
   }
 }
 
+function renderTrashNodes() {
+  trashConten.innerHTML = "";
+  for (let trashnotsindex = 0; trashnotsindex < trashNoats.length; trashnotsindex++) {
+    trashConten.innerHTML += getTrashNoteTamplate(trashnotsindex);
+  }
+}
+
 function getNoteTamplate(notsindex) {
-  return `<p>+ ${notes[notsindex]} <button onclick="removeItem(${notsindex})">x</button></p>`;
+  return `<p>+ ${titelNots[notsindex]} -> ${notes[notsindex]} <button onclick="removeItem(${notsindex})">x</button></p>`;
+}
+
+function getTrashNoteTamplate(trashnotsindex) {
+  return `<p>+ ${trashNoatsTitels[trashnotsindex]} -> ${trashNoats[trashnotsindex]} <button onclick="removeTrashItem(${trashnotsindex})">x</button></p></p>`;
 }
 
 function addnote() {
   notes.push(noteInput.value);
+  titelNots.push(titelInput.value);
   mainConten.innerHTML = "";
+
   renderNodes();
   noteInput.value = "";
 }
 
 function removeItem(notsindex) {
-  notes.splice(notsindex, 1);
+  let trashNoat = notes.splice(notsindex, 1);
+  trashNoats.push(trashNoat);
+
+  let trashNoatTitel = titelNots.splice(notsindex, 1);
+  trashNoatsTitels.push(trashNoatTitel);
+
   renderNodes();
+  renderTrashNodes();
+}
+
+function removeTrashItem(trashnotsindex) {
+  let removTrashNoat = trashNoats.splice(trashnotsindex, 1);
+  newtrashNoats.push(removTrashNoat);
+
+  let removTrashNoatTitel = trashNoats.splice(trashnotsindex, 1);
+  newtrashNoatsTitels.push(removTrashNoatTitel);
+
+  renderTrashNodes();
 }
